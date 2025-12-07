c81ad45c42cf69b05f858072852bc2145c0cd76c
Alexander Kravchenko <alexandercray05@gmail.com>
2025-12-07 12:35:21 +0300

    feat: add redis.go

---
+++ analytics-service/internal/infrastructure/redis/redis.go
@@ -0,0 +1,46 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/cg-2025-crutch/backend/analytics-service/internal/config"
+	"github.com/cg-2025-crutch/backend/analytics-service/internal/infrastructure/log"
+	"github.com/redis/rueidis"
+	"go.uber.org/zap"
+)
+
+func NewRedisClient(ctx context.Context, conf config.RedisConfig) (rueidis.Client, error) {
+	l := log.FromContext(ctx)
+
+	cl, err := rueidis.NewClient(rueidis.ClientOption{
+		InitAddress: []string{conf.Host},
+		Password:    conf.Password,
+	})
+	if err == nil {
+		l.Infof("connected to redis on host: %v", conf.Host)
+		return cl, nil
+	}
+	l.Error("failed to connect to redis, trying to reconnect", zap.Error(err))
+	ticker := time.NewTicker(1 * time.Second)
+	ctxStop, cancel := context.WithTimeout(ctx, conf.ConnDeadline)
+	defer cancel()
+
+	for {
+		select {
+		case <-ctxStop.Done():
+			return nil, fmt.Errorf("failed to connect to redis after %s", conf.ConnDeadline.String())
+		case <-ticker.C:
+			cl, err = rueidis.NewClient(rueidis.ClientOption{
+				InitAddress: []string{conf.Host},
+				Password:    conf.Password,
+			})
+			if err == nil {
+				l.Infof("connected to redis on host: %v", conf.Host)
+				return cl, nil
+			}
+			l.Errorf("failed to connect to redis, reconnecting...: %v", err)
+		}
+	}
+}
